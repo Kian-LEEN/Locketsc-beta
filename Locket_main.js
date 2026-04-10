492bf9fa4fa8c21685185881189be3e55b2d233c
Kian-LEEN <cp.enderboy@gmail.com>
2026-04-10 16:14:08 +0700

    Cập nhật code mới cho Locketsc-beta

--- Locket_main.js
+++ Locket_main.js
@@ -4,23 +4,7 @@ const mapping = {
   'Locket': ['Gold']
 };
 // =========   Phần cố định  ========= // 
-// =========  Fake Location: California, USA - Modified by KhanhLN ========= //
-var ua=$request.headers["User-Agent"]||$request.headers["user-agent"],obj=JSON.parse($response.body);obj.Attention="Chúc mừng bạn! Vui lòng không bán hoặc chia sẻ cho người khác!";var ohoang7={is_sandbox:!1,ownership_type:"PURCHASED",billing_issues_detected_at:null,period_type:"normal",expires_date:"2099-12-18T01:04:17Z",grace_period_expires_date:null,unsubscribe_detected_at:null,original_purchase_date:"2024-07-28T01:04:18Z",purchase_date:"2024-07-28T01:04:17Z",store:"app_store"},vuong2023={grace_period_expires_date:null,purchase_date:"2024-07-28T01:04:17Z",product_identifier:"com.ohoang7.premium.yearly",expires_date:"2099-12-18T01:04:17Z"};
+var ua=$request.headers["User-Agent"]||$request.headers["user-agent"],obj=JSON.parse($response.body);obj.Attention="Chúc mừng bạn! Vui lòng không bán hoặc chia sẻ cho người khác!";var kianleen={is_sandbox:!1,ownership_type:"PURCHASED",billing_issues_detected_at:null,period_type:"normal",expires_date:"2099-12-18T01:04:17Z",grace_period_expires_date:null,unsubscribe_detected_at:null,original_purchase_date:"2024-07-28T01:04:18Z",purchase_date:"2024-07-28T01:04:17Z",store:"app_store"},kianleen={grace_period_expires_date:null,purchase_date:"2024-07-28T01:04:17Z",product_identifier:"com.kianleen.premium.yearly",expires_date:"2099-12-18T01:04:17Z"};
 
-// === Fake Location: Los Angeles, California, USA ===
-if(obj.subscriber){
-  obj.subscriber.store_country="US";
-  if(!obj.subscriber.subscriber_attributes)obj.subscriber.subscriber_attributes={};
-  obj.subscriber.subscriber_attributes["$countryCode"]={value:"US",updated_at_ms:Date.now()};
-  obj.subscriber.subscriber_attributes["$gpsAdCountry"]={value:"US",updated_at_ms:Date.now()};
-  obj.subscriber.subscriber_attributes["$ipCountry"]={value:"US",updated_at_ms:Date.now()};
-  obj.subscriber.subscriber_attributes["$locationLatitude"]={value:"34.0522",updated_at_ms:Date.now()};
-  obj.subscriber.subscriber_attributes["$locationLongitude"]={value:"-118.2437",updated_at_ms:Date.now()};
-  obj.subscriber.subscriber_attributes["$locationCity"]={value:"Los Angeles",updated_at_ms:Date.now()};
-  obj.subscriber.subscriber_attributes["$locationState"]={value:"California",updated_at_ms:Date.now()};
-  obj.subscriber.subscriber_attributes["$locationZip"]={value:"90001",updated_at_ms:Date.now()};
-  obj.subscriber.subscriber_attributes["$locale"]={value:"en_US",updated_at_ms:Date.now()};
-  obj.subscriber.subscriber_attributes["$timezone"]={value:"America/Los_Angeles",updated_at_ms:Date.now()};
-}
 
-const match=Object.keys(mapping).find(e=>ua.includes(e));if(match){let[e,s]=mapping[match];s?(vuong2023.product_identifier=s,obj.subscriber.subscriptions[s]=ohoang7):obj.subscriber.subscriptions["com.ohoang7.premium.yearly"]=ohoang7,obj.subscriber.entitlements[e]=vuong2023}else obj.subscriber.subscriptions["com.ohoang7.premium.yearly"]=ohoang7,obj.subscriber.entitlements.pro=vuong2023;$done({body:JSON.stringify(obj)});
+const match=Object.keys(mapping).find(e=>ua.includes(e));if(match){let[e,s]=mapping[match];s?(kianleen.product_identifier=s,obj.subscriber.subscriptions[s]=kianleen):obj.subscriber.subscriptions["com.kianleen.premium.yearly"]=kianleen,obj.subscriber.entitlements[e]=kianleen}else obj.subscriber.subscriptions["com.kianleen.premium.yearly"]=kianleen,obj.subscriber.entitlements.pro=kianleen;$done({body:JSON.stringify(obj)});
--- deleteHeader.js
+++ deleteHeader.js
@@ -1,6 +1,5 @@
 /***********************************************
 > deleteHeader by Kian-LEEN
-> Fake Location: California, USA - Modified by KhanhLN
 ***********************************************/	
 
 const version = 'V1.0.3';
@@ -12,16 +11,5 @@ var modifiedHeaders = $request.headers;
 // === Xóa ETag để buộc server trả response đầy đủ ===
 setHeaderValue(modifiedHeaders, "X-RevenueCat-ETag", "");
 
-// === Fake Location: Los Angeles, California, USA ===
-setHeaderValue(modifiedHeaders, "Accept-Language", "en-US,en;q=0.9");
-setHeaderValue(modifiedHeaders, "X-Forwarded-For", "172.58.142.1");
-setHeaderValue(modifiedHeaders, "CF-IPCountry", "US");
-setHeaderValue(modifiedHeaders, "X-Client-Locale", "en_US");
-setHeaderValue(modifiedHeaders, "X-Client-Timezone", "America/Los_Angeles");
-setHeaderValue(modifiedHeaders, "X-Storefront-Country", "US");
-setHeaderValue(modifiedHeaders, "X-AppStore-Country", "US");
-setHeaderValue(modifiedHeaders, "X-Client-Country", "US");
-setHeaderValue(modifiedHeaders, "X-Client-Region", "US");
-setHeaderValue(modifiedHeaders, "X-Custom-Location", "34.0522,-118.2437");
 
 $done({ headers: modifiedHeaders });
